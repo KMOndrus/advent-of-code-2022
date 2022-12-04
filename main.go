a2037d64208c8b4bceb01f60e0e798adc7fcab84
Keri <kloring@gmail.com>
2022-12-04 10:45:06 -0700

    Day 2 Puzzle 1 working with sample input

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent-of-code-2022/day1"
+	"advent-of-code-2022/day2"
 	"fmt"
 )
 
@@ -11,4 +12,7 @@ func main() {
 
 	day1Puzzle2 := day1.Puzzle2()
 	fmt.Printf("Day 1 Puzzle 2 Result: %v\n", day1Puzzle2)
+
+	day2Puzzle1 := day2.Puzzle1()
+	fmt.Printf("Day 2 Puzzle 1 Result: %d\n", day2Puzzle1)
 }
