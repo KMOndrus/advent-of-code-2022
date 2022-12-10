88ff691f35edaec44da2202c6d22cd69187b6e0a
Keri <kloring@gmail.com>
2022-12-10 11:28:22 -0700

    Continued to refactor.

--- day2/puzzles.go
+++ day2/puzzles.go
@@ -13,7 +13,6 @@ const (
 	Rock Move = iota
 	Paper
 	Scissors
-	NoMove
 )
 
 type WinStatus int
@@ -22,7 +21,6 @@ const (
 	Win WinStatus = iota
 	Loss
 	Draw
-	NoStatus
 )
 
 func Puzzles(puzzleNumber int) int {
@@ -39,6 +37,100 @@ func Puzzles(puzzleNumber int) int {
 		finalScore += roundScore
 	}
 	return finalScore
+}
+
+func getScoreForRound(codedRoundInput string, puzzleNumber int) int {
+	var myMove Move
+	var winStatus WinStatus
+
+	firstPartOfInput, secondPartOfInput := getInputParts(codedRoundInput)
+	opponentMove := getOpponentMoveFromCodedInput(firstPartOfInput)
+
+	if puzzleNumber == 1 {
+		myMove = getMyMoveFromCodedInput(secondPartOfInput)
+		winStatus = determineWinStatus(myMove, opponentMove)
+	} else {
+		winStatus = getWinStatusFromCodedInput(secondPartOfInput)
+		myMove = determineMyMove(opponentMove, winStatus)
+	}
+
+	return calculateRoundScore(myMove, winStatus)
+}
+
+func getInputParts(fullInput string) (firstPart, secondPart string) {
+	bothParts := strings.Split(fullInput, " ")
+	if len(bothParts) != 2 {
+		log.Fatal("Incorrect input format")
+	}
+
+	firstPart = bothParts[0]
+	secondPart = bothParts[1]
+	return firstPart, secondPart
+}
+
+func getOpponentMoveFromCodedInput(codedInput string) Move {
+	var opponentMove Move
+	switch codedInput {
+	case "A":
+		opponentMove = Rock
+	case "B":
+		opponentMove = Paper
+	case "C":
+		opponentMove = Scissors
+	default:
+		log.Fatal("The opponent used an unknown gesture")
+	}
+	return opponentMove
+}
+
+func getMyMoveFromCodedInput(codedInput string) Move {
+	var myMove Move
+	switch codedInput {
+	case "X":
+		myMove = Rock
+	case "Y":
+		myMove = Paper
+	case "Z":
+		myMove = Scissors
+	default:
+		log.Fatal("I used an unknown gesture")
+	}
+	return myMove
+}
+
+func getWinStatusFromCodedInput(codedInput string) WinStatus {
+	var winStatus WinStatus
+	switch codedInput {
+	case "X":
+		winStatus = Loss
+	case "Y":
+		winStatus = Draw
+	case "Z":
+		winStatus = Win
+	default:
+		log.Fatal("An unknown win status was provided")
+	}
+	return winStatus
+}
+
+func determineWinStatus(myMove, opponentMove Move) WinStatus {
+	if myMove == opponentMove {
+		return Draw
+	}
+
+	if myMove == Rock && opponentMove == Scissors {
+		return Win
+	}
+
+	if myMove == Paper && opponentMove == Rock {
+		return Win
+	}
+
+	if myMove == Scissors && opponentMove == Paper {
+		return Win
+	}
+
+	return Loss
 }
 
 func determineMyMove(opponentMove Move, status WinStatus) Move {
@@ -67,55 +159,6 @@ func determineMyMove(opponentMove Move, status WinStatus) Move {
 	return opponentMove
 }
 
-func getRoundDetails(moveString string) (opponentMove Move, winStatus WinStatus) {
-	roundDetails := strings.Split(moveString, " ")
-	if len(roundDetails) != 2 {
-		log.Fatal("Not the right number of details for the round")
-	}
-
-	switch roundDetails[0] {
-	case "A":
-		opponentMove = Rock
-	case "B":
-		opponentMove = Paper
-	case "C":
-		opponentMove = Scissors
-	default:
-		opponentMove = NoMove
-	}
-
-	switch roundDetails[1] {
-	case "X":
-		winStatus = Loss
-	case "Y":
-		winStatus = Draw
-	case "Z":
-		winStatus = Win
-	default:
-		winStatus = NoStatus
-	}
-
-	if winStatus == NoStatus || opponentMove == NoMove {
-		log.Fatal("Some of the round details are invalid")
-	}
-	return opponentMove, winStatus
-}
-
-func getScoreForRound(moveString string, puzzleNumber int) int {
-	var opponentMove, myMove Move
-	var winStatus WinStatus
-
-	if puzzleNumber == 1 {
-		opponentMove, myMove = getMovesForRound(moveString)
-		winStatus = getWinStatus(myMove, opponentMove)
-	} else {
-		opponentMove, winStatus = getRoundDetails(moveString)
-		myMove = determineMyMove(opponentMove, winStatus)
-	}
-
-	return calculateRoundScore(myMove, winStatus)
-}
-
 func calculateRoundScore(myMove Move, status WinStatus) int {
 	roundScore := 0
 
@@ -136,58 +179,4 @@ func calculateRoundScore(myMove Move, status WinStatus) int {
 	}
 
 	return roundScore
-}
-
-func getWinStatus(myMove, opponentMove Move) WinStatus {
-	if myMove == opponentMove {
-		return Draw
-	}
-
-	if myMove == Rock && opponentMove == Scissors {
-		return Win
-	}
-
-	if myMove == Paper && opponentMove == Rock {
-		return Win
-	}
-
-	if myMove == Scissors && opponentMove == Paper {
-		return Win
-	}
-
-	return Loss
-}
-
-func getMovesForRound(moveString string) (opponentMove, myMove Move) {
-	bothMoves := strings.Split(moveString, " ")
-	if len(bothMoves) != 2 {
-		log.Fatal("Not the right number of players for the round")
-	}
-
-	switch bothMoves[0] {
-	case "A":
-		opponentMove = Rock
-	case "B":
-		opponentMove = Paper
-	case "C":
-		opponentMove = Scissors
-	default:
-		opponentMove = NoMove
-	}
-
-	switch bothMoves[1] {
-	case "X":
-		myMove = Rock
-	case "Y":
-		myMove = Paper
-	case "Z":
-		myMove = Scissors
-	default:
-		myMove = NoMove
-	}
-
-	if myMove == NoMove || opponentMove == NoMove {
-		log.Fatal("Somebody used an unknown gesture!")
-	}
-	return opponentMove, myMove
 }
